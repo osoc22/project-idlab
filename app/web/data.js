58ef8f8926b161c4e683d1fcc20d0d4b43ef752d
Denperidge <denperidge@gmail.com>
2022-07-06 13:54:23 +0200

    Created data.js to handle,,, data

---
+++ app/web/data.js
@@ -0,0 +1,6 @@
+import { fetch } from '@inrupt/solid-client-authn-browser';  // See https://docs.inrupt.com/developer-tools/javascript/client-libraries/tutorial/read-write-data/
+import { createSolidDataset, getSolidDataset, fromRdfJsDataset } from "@inrupt/solid-client";
+
+
+
+
