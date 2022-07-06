e81f07edadef8d444c7ca9afb558cf4d57a352b1
abel (tudelft) <a.debruijn-3@student.tudelft.nl>
2022-07-06 10:07:47 +0200

    Init svelte

---
+++ svelte-app/src/routes/login.svelte
@@ -0,0 +1,33 @@
+<div class="login">
+	<h1>Welcome to your calendar pod</h1>
+	<div class="actions">
+		<button class="login">Login</button>
+		<button class="register">Register</button>
+	</div>
+</div>
+
+<style>
+	h1 {
+		font-size: 3em;
+		margin: 0.5em 0;
+	}
+
+	.login {
+		margin: 10rem auto;
+	}
+
+	.actions {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	button {
+		background: #fff;
+		border: 1px solid #ccc;
+		border-radius: 3px;
+		color: #333;
+		font-size: 1.2em;
+		padding: 0.5em 1em;
+		margin: 0.5em 0;
+	}
+</style>
