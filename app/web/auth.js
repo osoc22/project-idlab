31b6caadaa93ab6afdd9d5478c4c1e98181619e7
Denperidge <denperidge@gmail.com>
2022-07-06 13:36:01 +0200

    Login session now gets restored

--- app/web/auth.js
+++ app/web/auth.js
@@ -9,7 +9,9 @@ import {
 // Implementation based on https://docs.inrupt.com/developer-tools/javascript/client-libraries/tutorial/authenticate-browser/
 async function handleLogin() {
     // If being redirected from login
-    await handleIncomingRedirect();
+    await handleIncomingRedirect({
+        restorePreviousSession: true
+    });
 
     if (!getDefaultSession().info.isLoggedIn) {
         await login({
