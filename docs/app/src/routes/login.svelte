1250e9485b5ed2227d20ae09cb781eae2c2dbd6f
Denperidge <Denperidge@users.noreply.github.com>
2022-07-12 13:48:39 +0000

    Deploying to master from @ osoc22/project-idlab@b8fcee1ab123d0fa1d402236f3cc812d629f7d78 🚀

---
+++ docs/app/src/routes/login.svelte
@@ -0,0 +1,30 @@
+<div class="login">
+	<h1>Welcome to your calendar pod</h1>
+	<div class="actions">
+		<button class="signin" on:click={() => console.log('sign in')}>Sign in</button>
+		<button class="register" on:click={() => console.log('register')}>Register</button>
+	</div>
+</div>
+
+<style>
+	.login {
+		margin: 10rem auto;
+		width: min-content;
+	}
+
+	.actions {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	button {
+		background: #fff;
+		border: 1px solid #ccc;
+		border-radius: 3px;
+		color: #333;
+		font-size: 1.2em;
+		padding: 1em 2em;
+		margin: 1rem 4rem;
+		cursor: pointer;
+	}
+</style>
