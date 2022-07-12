11d2f6f483560668822b251b94f7875b98b7c5e0
Denperidge <denperidge@gmail.com>
2022-07-12 15:23:48 +0200

    Run dev & build now work with svelte & solidclient

--- app/svelte/vite.config.js
+++ app/svelte/vite.config.js
@@ -1,11 +1,34 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import builtins from "rollup-plugin-node-builtins";
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	define: {
-		global: {}
-	}
+	optimizeDeps: {
+		esbuildOptions: {
+		  plugins: [
+			NodeGlobalsPolyfillPlugin({
+			  process: true,
+			  buffer: true,
+			}),
+			NodeModulesPolyfillPlugin(),
+		  ],
+		},
+	},
+	build: {
+		rollupOptions: {
+		  plugins: [
+			// Enable rollup polyfills plugin
+			// used during production bundling
+			builtins(),
+			rollupNodePolyFill(),
+		  ],
+		},
+	},
+
 };
 
 export default config;
