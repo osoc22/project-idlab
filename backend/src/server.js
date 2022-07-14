d5aef35ad4c0c6d11266c5f7c4855cfdfcb48361
Matei9721 <matei.penca1@gmail.com>
2022-07-14 13:07:20 +0200

    Documented code and deleted

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import Fastify from 'fastify'
 import cors from '@fastify/cors';
 
+// Import method to convert JSON to linked-data (JSON-LD)
 import {convertData} from './rml-mapper.js'
 
 // Run the server!
@@ -36,32 +37,33 @@ const start = async () => {
         const locationRes = await fetch(`https://nominatim.openstreetmap.org/search?q=${request.params.city}&format=json`);
         const locationJson = await locationRes.json();
 
-
+        // Check if the response is an array
         if(!Array.isArray((locationJson))) {
             return reply.status(500);
         }
-
+        // Make sure there is data in the response
         if(locationJson.length === 0) {
             return reply.status(404);
         }
 
-        // First match is probably the best so we pick that
+        // Worth pointing out that we pick the first entry in the array of options as it
+        // is most likely to be what the end-user was looking forward.
         const { lat, lon } = locationJson[0]
-
-        console.log(lat,lon)
 
         // Retrieve the weather data in JSON format.
         const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/onecall?units=metric&lat=${lat}&lon=${lon}&exclude=hourly,minutely,current&appid=${process.env.WEATHER_KEY}`);
         const weatherJson = await weatherRes.json();
+        // Convert the JSON to JSON-LD
         const weather_JsonLD = await convertData(weatherJson);
-        //console.log(weather_JsonLD)
+
+        // Serve the JSON-LD back.
         return reply
             .status(200)
             .header('content-type', 'application/ld+json')
             .send(weather_JsonLD.output);
-        //return weather_JsonLD.output
     })
 
+    // Define what port the server listens to
     await fastify.listen({
         port: process.env.PORT,
         host: process.env.HOST
