308a5ec8b21a7c142cc50eda8c8455029bf20a3a
yustarandomname <yustarandomname@users.noreply.github.com>
2022-07-19 13:23:34 +0000

    Deploying to gh-pages from @ osoc22/project-idlab@a7c48e7785ed25b31fa3e549ec04322868bbc885 🚀

--- docs/app/internal/immutable/pages/index.svelte-9e581506.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as e,s as o}from"../chunks/index-c908d7e2.js";import{b as a}from"../chunks/paths-1c47712a.js";async function i(){return{status:302,redirect:a+"/upcomming"}}class l extends t{constructor(s){super(),e(this,s,null,null,o,{})}}export{l as default,i as load};
