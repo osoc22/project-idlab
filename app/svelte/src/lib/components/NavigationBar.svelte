ef077fe68cf4e93b9bc1f3d07ef5d0f2aebc5df9
Denperidge <denperidge@gmail.com>
2022-07-27 13:57:10 +0200

    Create new Event navbar change w @yustarandomname

--- app/svelte/src/lib/components/NavigationBar.svelte
+++ app/svelte/src/lib/components/NavigationBar.svelte
@@ -41,7 +41,7 @@
 	<!-- Main Content -->
 	<main class="grow p-5">
 		<div class="flex gap-4 justify-end mb-10">
-			<Button icon={Plus} on:click={modifyUnplannedActivity.new}>Create new event</Button>
+			<Button icon={Plus} on:click={modifyPlannedActivity.new}>Create new event</Button>
 
 			<Profile firstname="John" lastname="Doe" />
 		</div>
