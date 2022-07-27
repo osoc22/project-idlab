0b57b19e4511a28062bb5b0b698be8030c4ada5c
Denperidge <denperidge@gmail.com>
2022-07-27 11:41:21 +0200

    Run: Svelte can now be started, stopped & built

--- run.mjs
+++ run.mjs
@@ -5,6 +5,7 @@ import { existsSync } from 'fs';
 import { spawn } from 'child_process';
 import { request } from 'http';
 
+
 // Functions
 function appDir(appName) {
     return `app/${appName}`;
@@ -14,46 +15,88 @@ function isInstalled(appName) {
     return existsSync(`${appDir(appName)}/node_modules`);
 }
 function runCommand(cwd, command) {
-    spawn(command, {cwd: cwd, detached: true, shell: true});
+    if (typeof(command) != "function") {
+        return spawn(command, {cwd: cwd, detached: true, shell: true});
+    } else {
+        command();
+    }
+}
+
+function kill(pid){
+    if (process.platform.startsWith('win')) {
+        spawn(`taskkill `, ['/pid', pid, '/f', '/t'], {shell: true});
+    } else {
+        spawn(`kill -9 ${pid}`);
+    }
 }
 
 let httpOptions = {
     hostname: 'localhost',
     method: 'GET'
 }
-function portInUse(port) {
+async function portInUse(port) {
+    // Promise solution based off https://stackoverflow.com/a/37104225
     httpOptions.port = port;
-    try {
-        request(httpOptions)
-        return true;  // If the code gets to this point, theres a server running
-    } catch (err) {
-        if (err.code == "ECONNREFUSED") return false;
-        else console.log(err);
-    }
+    return await new Promise(resolve => {
+        let req = request(httpOptions)
+            .on('error', (err) => {
+                resolve(false);
+            }).on('response', (res) => {
+                resolve(true);
+            })
+        req.end();
+    })
 }
 
-function newAppPrompt(prompt, appName, command, statusText) {
+// The id can be anything: its a custom string used to kill the process down the line
+function newAppPrompt(prompt, appName, command, id="") {
     prompts.push({
-        "prompt": prompt,
         "cwd": appDir(appName),
         "command": command,
-        "statusText": statusText
+        "id": id
     });
+    console.log(`[${i}] ${prompt}`);
+    i++;
 }
 
-function listPrompts() {
+let i;
+let prompts;  // [{cwd, command}]
+async function listPrompts() {
+    prompts = []
+    i = 0;
+
     if (!isInstalled("svelte")) {
         newAppPrompt("Install Svelte/frontend", "svelte", "npm install");
     } else {
-        newAppPrompt("Install Svelte/frontend", "svelte", "npm install");
-        newAppPrompt("Install Svelte/frontend", "svelte", "npm install");
+        if (await portInUse(3333)) {
+            if (processes["sveltedev"]) {
+                newAppPrompt("Exit Svelte/frontend dev server", "svelte", ()=>{
+                    let pid = processes['sveltedev'].pid;
+                    kill(pid)
+                    //console.log(processes["sveltedev"].kill)
+                    //process.kill(
+                    //processes["sveltedev"].kill('SIGINT');
+                    //kill()
+                });
+            } else {
+                console.log("[ ] Port 3333 in use!")
+            }
+        } else {
+            newAppPrompt("Run svelte/frontend locally", "svelte", "npm run dev", "sveltedev");
+        }
+        newAppPrompt("Build svelte/frontend", "svelte", "npm run build");
     }
     
+    //if (!isInstalled("backend"))
+
+    // UI
     console.log("[q] Quit");
 }
 
 const rl = createInterface(stdin, stdout);
-let prompts = [];  // [{prompt, cwd, command}]
+rl.on('close', () => {
+    process.exit(0);
+});
 
 
 function parseAnswer(answer) {
@@ -64,15 +107,18 @@ function parseAnswer(answer) {
     if (answerInt != NaN && answerInt >= 0 && answerInt < prompts.length) {
         let choice = prompts[answerInt];
         console.log(choice);
-        runCommand(choice.cwd, choice.command);
+        let child = runCommand(choice.cwd, choice.command);
+        processes[choice.id] = child;
+
     } else if (answer.toLowerCase().startsWith("q")) {
-        return;
+        rl.close();  // This will also exit the application
     }
     main();
 }
 
-function main() {
-    listPrompts();
+let processes = {};
+async function main() {
+    await listPrompts();
     rl.question("meow: ", parseAnswer);
 }
 
